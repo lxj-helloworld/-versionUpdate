145c10138fd13a5373b4e00f459c7a8838bfe619
= <=>
2017-11-17 09:24:38 +0800

    错误秀爱

--- app/src/main/java/com/example/xiaojin20135/versionupdate/MainActivity.java
+++ app/src/main/java/com/example/xiaojin20135/versionupdate/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.xiaojin20135.versionupdate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.xiaojin20135.updatelibrary.UpdateChecker;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Log.d(TAG,"msg.obj = " + msg.obj.toString());
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        UpdateChecker updateChecker = new UpdateChecker(MainActivity.this,handler);
+        String checkUrl = "http://www.topscomm.com:5000/app_download/package/chpcyDebug/update.json" + "?t="+System.currentTimeMillis();
+        updateChecker.setCheckUrl(checkUrl);
+        updateChecker.setShowAlert(false);
+        updateChecker.checkForUpdates();
     }
 }
